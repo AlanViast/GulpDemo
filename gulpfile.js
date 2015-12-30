df2788fc8c3c28d3a56daef35c3f58d331e96d12
AlanViast <AlanViast@gmail.com>
2015-12-30 16:31:46 +0800

    base project struts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('default', function() {
+
+gulp.task('default',['compress'], function() {
   // place code for your default task here
   // });
 });
@@ -8,3 +12,28 @@ gulp.task('default', function() {
 gulp.task('hello', function(){
   console.log('Hell World');
 });
+
+gulp.task('compress', ['minify', 'minify-css'], function(){
+
+});
+
+gulp.task('minify', function(){
+  gulp.src('src/js/**/*.js')
+    .pipe(minify())   // 代码压缩
+    .pipe(uglify())   // 代码混淆
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('src/css/**/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+var watcher = function(){
+  gulp.watch('js/**/*.js', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+};
+
+gulp.task('watch', ['compress'], watcher);
---
+++ src/js/hello.js
@@ -0,0 +1,7 @@
+
+
+var testFunction = function(){
+  var testA = 10;
+  var testB = 20;
+  console.log(testA, testB);
+}
